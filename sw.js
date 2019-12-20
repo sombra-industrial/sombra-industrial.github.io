93a3665d7c26d3d127ad430558fc5a7f01ec08ff
sombra-industrial <58218153+sombra-industrial@users.noreply.github.com>
2019-12-20 14:33:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// ‘use strict’;
-importScripts(‘sw-toolbox.js’); toolbox.precache([“index.html”,”css/style.css”]); toolbox.router.get(‘/things/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+"use strict";
+importScripts("sw-toolbox.js"); toolbox.precache(["index.html","css/style.css"]); toolbox.router.get("/things/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
